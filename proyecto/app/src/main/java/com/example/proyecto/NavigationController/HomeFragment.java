d0d287983e94df785c70010bf86e8779dbc1dd52
gerardlloret <gerardlloreteij@gmail.com>
2020-04-06 20:42:00 +0200

    item_oferta

--- proyecto/app/src/main/java/com/example/proyecto/NavigationController/HomeFragment.java
+++ proyecto/app/src/main/java/com/example/proyecto/NavigationController/HomeFragment.java
@@ -3,12 +3,14 @@ package com.example.proyecto.NavigationController;
 
 import android.os.Bundle;
 
+import androidx.appcompat.widget.AppCompatTextView;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.example.proyecto.R;
 
@@ -17,6 +19,7 @@ import com.example.proyecto.R;
  */
 public class HomeFragment extends Fragment {
 
+    AppCompatTextView fragHomeTest;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -28,7 +31,11 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         Log.d("gla", "HOME: ");
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        fragHomeTest = view.findViewById(R.id.fragHomeTest);
+        fragHomeTest.setText("Funciona");
+        //return inflater.inflate(R.layout.fragment_home, container, false);
+        return view;
     }
 
 }
