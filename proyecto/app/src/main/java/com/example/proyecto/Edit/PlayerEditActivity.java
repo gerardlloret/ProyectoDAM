5acda60637b388242f79b886656d460c4645f5f1
gerardlloret <gerardlloreteij@gmail.com>
2020-05-15 16:51:41 +0200

    player edit faltan peticiones backend

--- proyecto/app/src/main/java/com/example/proyecto/Edit/PlayerEditActivity.java
+++ proyecto/app/src/main/java/com/example/proyecto/Edit/PlayerEditActivity.java
@@ -3,13 +3,21 @@ package com.example.proyecto.Edit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
+import com.example.proyecto.Handler.Manager;
 import com.example.proyecto.R;
+
+import java.io.IOException;
+
+import static com.example.proyecto.Handler.Manager.BitMapToString;
 
 public class PlayerEditActivity extends AppCompatActivity {
 
@@ -18,6 +26,7 @@ public class PlayerEditActivity extends AppCompatActivity {
     ImageButton btnAPEgalery;
     private static final int imgGalery = 100;
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,4 +65,21 @@ public class PlayerEditActivity extends AppCompatActivity {
         Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
         startActivityForResult(gallery, imgGalery);
     }
+
+    //Sobreescribim onActivityResult per poder colocar al imageView la foto realitzada per l'usuari o la seleccionada a la galeria
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            ivAPEimage.setImageBitmap(imageBitmap);
+        }
+        if(resultCode == RESULT_OK && requestCode == imgGalery){
+            imageUri = data.getData();
+            ivAPEimage.setImageURI(imageUri);
+        }
+    }
+
+
 }
--- proyecto/app/src/main/java/com/example/proyecto/Handler/Manager.java
+++ proyecto/app/src/main/java/com/example/proyecto/Handler/Manager.java
@@ -1,6 +1,9 @@
 package com.example.proyecto.Handler;
 
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,6 +16,7 @@ import com.android.volley.toolbox.Volley;
 import com.example.proyecto.Model.Token;
 import com.google.gson.Gson;
 
+import java.io.ByteArrayOutputStream;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Matcher;
@@ -28,4 +32,25 @@ public class Manager {
         }
         return false;
     }
+
+    //Aquest metode serveix per convertir Bitmap a String
+    public static String BitMapToString(Bitmap bitmap){
+        ByteArrayOutputStream baos=new  ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG,100, baos);
+        byte [] b=baos.toByteArray();
+        String temp= Base64.encodeToString(b, Base64.DEFAULT);
+        return temp;
+    }
+    //Aquest metode serveix per convertir String a Bitmap
+    public static Bitmap StringToBitMap(String encodedString){
+        try {
+            byte [] encodeByte=Base64.decode(encodedString,Base64.DEFAULT);
+            Bitmap bitmap= BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.length);
+            return bitmap;
+        } catch(Exception e) {
+            e.getMessage();
+            return null;
+        }
+    }
+
 }
